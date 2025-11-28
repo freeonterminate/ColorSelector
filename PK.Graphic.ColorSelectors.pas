6bffd8c431b6019c4ead6be934cc5fdf418dba7a
HOSOKAWA Jun <sec256@gmail.com>
2025-11-28 20:59:09 +0900

    一応全て動作

--- PK.Graphic.ColorSelectors.pas
+++ PK.Graphic.ColorSelectors.pas
@@ -20,15 +20,17 @@ type
   THueCursor = class;
   TSVCursor = class;
   TRectCursor = class;
+  TCellCursor = class;
 
   TCustomSelector = class(TControl)
   private var
     FBase: TBitmap;
     FBaseColor: TAlphaColor;
     FColor: TAlphaColor;
+    FNoEvent: Boolean;
     FOnChange: TColorChangeEvent;
   private
-    procedure SetBaseColor(const AValue: TAlphaColor);
+    procedure SetBaseColor(const AColor: TAlphaColor);
   protected
     procedure Resize; override;
     procedure Paint; override;
@@ -39,12 +41,16 @@ type
 
     procedure DoChange;
 
-    procedure SetColor(const AValue: TAlphaColor); virtual;
+    procedure SetColor(const AColor: TAlphaColor); virtual;
 
     property Base: TBitmap read FBase;
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
+    procedure AfterConstruction; override;
+
+    procedure SetColorWihtoutEvent(const AColor: TAlphaColor);
+
     property BaseColor: TAlphaColor
       read FBaseColor
       write SetBaseColor
@@ -115,6 +121,7 @@ type
   TRectSelector = class(TCustomSelector)
   private const
     CURSOR_RATIO = 0.07;
+    CURSOR_MINIMUM_SIZE = 8;
   private var
     FHue: Single;
     FSat: Single;
@@ -125,7 +132,7 @@ type
   protected
     procedure Resize; override;
     procedure Draw(const ACanvas: TCanvas); override;
-    procedure SetColor(const Value: TAlphaColor); override;
+    procedure SetColor(const AColor: TAlphaColor); override;
 
     procedure MouseDown(
       AButton: TMouseButton;
@@ -137,14 +144,80 @@ type
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
-  published
-    property Color: TAlphaColor read FColor write SetColor;
+  end;
+
+  TCustomCellSelector = class(TCustomSelector)
+  private const
+    CELL_CURSOR_RATIO = 0.65; // カーソルサイズ比（セル短辺に対する）
+  private
+    FCols: Integer;
+    FRows: Integer;
+    FCellRect: TRectF;
+    FCellWidth: Single;
+    FCellHeight: Single;
+    FSelCol: Integer;
+    FSelRow: Integer;
+    FCursor: TCellCursor;
+  private
+    procedure SetGridSize(const ACols, ARows: Integer);
+  protected
+    function GetCellColor(ACol, ARow: Integer): TAlphaColor; virtual; abstract;
+    procedure SetCursorPosBySelected;
+
+    procedure Resize; override;
+    procedure Draw(const ACanvas: TCanvas); override;
+    procedure SetColor(const AValue: TAlphaColor); override;
+
+    procedure MouseDown(
+      AButton: TMouseButton;
+      AShift: TShiftState;
+      AX, AY: Single); override;
+    procedure MouseMove(
+      AShift: TShiftState;
+      AX, AY: Single); override;
+
+    procedure CellFromPoint(
+      const AX, AY: Single;
+      out ACol, ARow: Integer); virtual;
+
+    procedure SetSelectedCell(
+      ACol, ARow: Integer); virtual;
+
+    property CellRect: TRectF read FCellRect;
+    property ColCount: Integer read FCols;
+    property RowCount: Integer read FRows;
+    property SelectedCol: Integer read FSelCol;
+    property SelectedRow: Integer read FSelRow;
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+  end;
+
+  T16CellSelector = class(TCustomCellSelector)
+  private const
+    COL_COUNT = 8;
+    ROW_COUNT = 2;
+  protected
+    function GetCellColor(ACol, ARow: Integer): TAlphaColor; override;
+  public
+    constructor Create(AOwner: TComponent); override;
+  end;
+
+  T128CellSelector = class(TCustomCellSelector)
+  private const
+    COL_COUNT = 16;
+    ROW_COUNT = 8;
+  protected
+    function GetCellColor(ACol, ARow: Integer): TAlphaColor; override;
+  public
+    constructor Create(AOwner: TComponent); override;
   end;
 
   // カーソル
   TSelectorCursor = class(TCircle)
   private const
-    CURSOR_THICKNESS_RATIO = 1 / 10;
+    CURSOR_THICKNESS_RATIO = 1 / 10; // Width の 1/10
+    CUSOR_THICKNESS_MINIMUM_SIZE = 3;
   private var
     FSelector: TCustomSelector;
   protected
@@ -172,6 +245,11 @@ type
     procedure MoveTo(const AX, AY: Single); override;
   end;
 
+  TCellCursor = class(TSelectorCursor)
+  protected
+    procedure MoveTo(const AX, AY: Single); override;
+  end;
+
 implementation
 
 uses
@@ -185,6 +263,12 @@ uses
 
 { TCustomSelector }
 
+procedure TCustomSelector.AfterConstruction;
+begin
+  inherited;
+  SetSize(320, 320);
+end;
+
 constructor TCustomSelector.Create(AOwner: TComponent);
 begin
   inherited;
@@ -192,7 +276,6 @@ begin
   FBaseColor := TAlphaColors.White;
 
   FBase := TBitmap.Create;
-  SetSize(320, 320);
 end;
 
 destructor TCustomSelector.Destroy;
@@ -204,7 +287,7 @@ end;
 
 procedure TCustomSelector.DoChange;
 begin
-  if Assigned(FOnChange) then
+  if (not FNoEvent) and Assigned(FOnChange) then
     FOnChange(Self, FColor);
 end;
 
@@ -261,20 +344,28 @@ begin
   PreDraw;
 end;
 
-procedure TCustomSelector.SetBaseColor(const AValue: TAlphaColor);
+procedure TCustomSelector.SetBaseColor(const AColor: TAlphaColor);
 begin
-  FBaseColor := AValue;
+  FBaseColor := AColor;
   PreDraw;
 end;
 
 
-procedure TCustomSelector.SetColor(const AValue: TAlphaColor);
+procedure TCustomSelector.SetColor(const AColor: TAlphaColor);
 begin
-  if FColor = AValue then
+  if FColor = AColor then
     Exit;
 
-  FColor := AValue;
+  FColor := AColor;
+
   DoChange;
+end;
+
+procedure TCustomSelector.SetColorWihtoutEvent(const AColor: TAlphaColor);
+begin
+  FNoEvent := True;
+  SetColor(AColor);
+  FNoEvent := False;
 end;
 
 { TCircleSelector }
@@ -694,6 +785,12 @@ end;
 
 { TRectSelector }
 
+procedure TRectSelector.CalcColor;
+begin
+  FColor := HSV2RGB(FHue, FSat, FVal);
+  DoChange;
+end;
+
 constructor TRectSelector.Create(AOwner: TComponent);
 begin
   // カーソルを先に作る（Parent は後で）
@@ -715,12 +812,6 @@ destructor TRectSelector.Destroy;
 begin
   FCursor.Free;
   inherited;
-end;
-
-procedure TRectSelector.CalcColor;
-begin
-  FColor := HSV2RGB(FHue, FSat, FVal);
-  DoChange;
 end;
 
 procedure TRectSelector.Draw(const ACanvas: TCanvas);
@@ -793,31 +884,330 @@ begin
   inherited;
 
   // カーソルサイズは短辺に対してちょっとだけ
-  var Size := Min(Width, Height) * CURSOR_RATIO;
-  if Size < 10 then
-    Size := 10;
-
+  var Size := Max(Min(Width, Height) * CURSOR_RATIO, CURSOR_MINIMUM_SIZE);
   FCursor.Update(Size);
 
   PreDraw;
 end;
 
-procedure TRectSelector.SetColor(const Value: TAlphaColor);
+procedure TRectSelector.SetColor(const AColor: TAlphaColor);
 begin
-  if FColor = Value then
+  if FColor = AColor then
     Exit;
 
-  FColor := Value;
+  FColor := AColor;
   RGB2HSV(FColor, FHue, FSat, FVal);
 
-  // Hue は 0..360 に正規化
-  Adjust360(FHue);
+  var R := LocalRect.Round;
+  var InnerW := Max(1.0, R.Width  - 1);
+  var InnerH := Max(1.0, R.Height - 1);
 
-  FSat := EnsureRange(FSat, 0, 1);
-  FVal := EnsureRange(FVal, 0, 1);
+  // X : Hue
+  var X  := R.Left + InnerW * FHue / 360;
 
-  CalcColor;
+  var Y: Single;
+  if Abs(FVal - 1) > Abs(FSat - 1) then
+  begin
+    // 下半分: S=1, V=1→0
+    Y := R.Top + (InnerH * 0.5) + (1 - FVal) * (InnerH * 0.5);
+  end
+  else
+  begin
+    // 上半分: V=1, S=0→1
+    Y := R.Top + FSat * (InnerH * 0.5);
+  end;
+
+  FCursor.SetBounds(
+    X - FCursor.Width  / 2,
+    Y - FCursor.Height / 2,
+    FCursor.Width,
+    FCursor.Height);  
 end;
+
+{ TCustomCellSelector }
+
+constructor TCustomCellSelector.Create(AOwner: TComponent);
+begin
+  inherited;
+
+  FCols := 1;
+  FRows := 1;
+  FSelCol := 0;
+  FSelRow := 0;
+
+  FCursor := TCellCursor.Create(Self);
+  FCursor.Parent := Self;
+end;
+
+destructor TCustomCellSelector.Destroy;
+begin
+  FCursor.Free;
+  
+  inherited;
+end;
+
+procedure TCustomCellSelector.SetGridSize(const ACols, ARows: Integer);
+begin
+  FCols := Max(1, ACols);
+  FRows := Max(1, ARows);
+
+  // セルサイズを再計算して描画更新
+  Resize;
+end;
+
+procedure TCustomCellSelector.Resize;
+begin
+  inherited;
+
+  // LocalRect 全体をセル領域として使う
+  FCellRect := LocalRect;
+
+  var W := Max(1.0, FCellRect.Width);
+  var H := Max(1.0, FCellRect.Height);
+
+  FCellWidth := W / FCols;
+  FCellHeight := H / FRows;
+
+  // カーソルのサイズ更新
+  var Size := Min(FCellWidth, FCellHeight) * CELL_CURSOR_RATIO;
+  if Size < 8 then
+    Size := 8;
+
+  FCursor.Update(Size);
+
+  // 現在の選択セルの中心にカーソルを再配置
+  SetCursorPosBySelected;
+
+  // セル描画更新
+  PreDraw;
+end;
+
+procedure TCustomCellSelector.Draw(const ACanvas: TCanvas);
+begin
+  inherited; // BaseColor でクリア
+
+  ACanvas.Fill.Kind := TBrushKind.Solid;
+  ACanvas.Stroke.Kind := TBrushKind.Solid;
+  ACanvas.Stroke.Thickness := 1;
+  ACanvas.Stroke.Color := FBaseColor xor $00FFFFFF; // 反転色で枠線
+
+  for var Row := 0 to FRows - 1 do
+  begin
+    for var Col := 0 to FCols - 1 do
+    begin
+      var CellR :=
+        RectF(
+          FCellRect.Left + Col * FCellWidth,
+          FCellRect.Top + Row * FCellHeight,
+          FCellRect.Left + (Col + 1) * FCellWidth,
+          FCellRect.Top + (Row + 1) * FCellHeight
+        );
+
+      ACanvas.Fill.Color := GetCellColor(Col, Row);
+      ACanvas.FillRect(CellR, 0, 0, [], 1);
+      ACanvas.DrawRect(CellR, 0, 0, [], 1);
+    end;
+  end;
+end;
+
+procedure TCustomCellSelector.CellFromPoint(
+  const AX, AY: Single; 
+  out ACol, ARow: Integer);
+begin
+  var X := EnsureRange(AX, FCellRect.Left, FCellRect.Right - 0.001);
+  var Y := EnsureRange(AY, FCellRect.Top,  FCellRect.Bottom - 0.001);
+
+  var LocalX := X - FCellRect.Left;
+  var LocalY := Y - FCellRect.Top;
+
+  ACol := Trunc(LocalX / FCellWidth);
+  ARow := Trunc(LocalY / FCellHeight);
+
+  ACol := EnsureRange(ACol, 0, FCols - 1);
+  ARow := EnsureRange(ARow, 0, FRows - 1);
+end;
+
+procedure TCustomCellSelector.SetSelectedCell(ACol, ARow: Integer);
+begin
+  ACol := EnsureRange(ACol, 0, FCols - 1);
+  ARow := EnsureRange(ARow, 0, FRows - 1);
+
+  FSelCol := ACol;
+  FSelRow := ARow;
+
+  FColor := GetCellColor(FSelCol, FSelRow);
+  DoChange;
+
+  SetCursorPosBySelected;
+
+  Invalidate;
+end;
+
+procedure TCustomCellSelector.MouseDown(
+  AButton: TMouseButton; AShift: TShiftState; AX, AY: Single);
+begin
+  inherited;
+
+  if (AButton = TMouseButton.mbLeft) and Pressed then
+    FCursor.MoveTo(AX, AY);
+end;
+
+procedure TCustomCellSelector.MouseMove(
+  AShift: TShiftState; AX, AY: Single);
+begin
+  inherited;
+
+  if Pressed then
+    FCursor.MoveTo(AX, AY);
+end;
+
+procedure TCustomCellSelector.SetColor(const AValue: TAlphaColor);
+begin
+  if FColor = AValue then
+    Exit;
+
+  var Target := AValue and $00_ff_ff_ff;
+  var Found := False;
+
+  for var Row := 0 to FRows - 1 do
+  begin
+    for var Col := 0 to FCols - 1 do
+    begin
+      if (GetCellColor(Col, Row) and $00_ff_ff_ff) = Target then
+      begin
+        SetSelectedCell(Col, Row);
+        Found := True;
+
+        Break;
+      end;
+    end;
+  end;
+
+  if Found then
+    FCursor.Visible := True
+  else
+    FCursor.Visible := False;
+end;
+
+procedure TCustomCellSelector.SetCursorPosBySelected;
+begin
+  var Center :=
+    PointF(
+      FCellRect.Left + (FSelCol + 0.5) * FCellWidth,
+      FCellRect.Top + (FSelRow + 0.5) * FCellHeight
+    );
+
+  FCursor.SetBounds(
+    Center.X - FCursor.Width / 2,
+    Center.Y - FCursor.Height / 2,
+    FCursor.Width,
+    FCursor.Height);
+end;
+
+{ T16CellSelector }
+
+constructor T16CellSelector.Create(AOwner: TComponent);
+begin
+  inherited;
+  // 8 列 × 2 行
+  SetGridSize(COL_COUNT, ROW_COUNT);
+end;
+
+function T16CellSelector.GetCellColor(ACol, ARow: Integer): TAlphaColor;
+const
+  COLORS:
+    array [0.. ROW_COUNT - 1, 0.. COL_COUNT - 1] of TAlphaColor =
+  (
+
+    ( // 0
+      ($ff_ff_ff_ff), ($ff_bb_bb_bb), ($ff_ff_ff_00), ($ff_ff_00_99),
+      ($ff_33_cc_00), ($ff_00_99_ff), ($ff_33_00_99), ($ff_99_66_33)
+    ),
+
+    ( // 1
+      ($ff_00_00_00), ($ff_66_66_66), ($ff_ff_66_00), ($ff_dd_00_00),
+      ($ff_00_66_00), ($ff_00_00_cc), ($ff_00_00_66), ($ff_66_33_00)
+    )
+  );
+begin
+  Result := COLORS[ARow, ACol];
+end;
+
+{ T128CellSelector }
+
+constructor T128CellSelector.Create(AOwner: TComponent);
+begin
+  inherited;
+  // 16 列 × 8 行
+  SetGridSize(16, 8);
+end;
+
+function T128CellSelector.GetCellColor(ACol, ARow: Integer): TAlphaColor;
+const
+  COLORS:
+    array [0.. ROW_COUNT - 1, 0.. COL_COUNT - 1] of TAlphaColor =
+  (
+    ( // 0
+      ($ff_ff_00_00), ($ff_ff_ff_00), ($ff_00_ff_00), ($ff_00_ff_ff),
+      ($ff_00_00_ff), ($ff_ff_00_ff), ($ff_ff_ff_ff), ($ff_e6_e6_e6),
+      ($ff_da_da_da), ($ff_cd_cd_cd), ($ff_c0_c0_c0), ($ff_b4_b4_b4),
+      ($ff_a8_a8_a8), ($ff_9a_9a_9a), ($ff_8d_8d_8d), ($ff_81_81_81)
+    ),
+
+    ( // 1
+      ($ff_ee_1d_24), ($ff_ff_f1_00), ($ff_00_a6_50), ($ff_00_ae_ef),
+      ($ff_2f_31_92), ($ff_ed_00_8c), ($ff_74_74_74), ($ff_66_66_66),
+      ($ff_59_59_59), ($ff_4b_4b_4b), ($ff_3e_3e_3e), ($ff_30_30_30),
+      ($ff_21_21_21), ($ff_13_13_13), ($ff_0a_0a_0a), ($ff_00_00_00)
+    ),
+
+    ( // 2
+      ($ff_f7_97_7a), ($ff_fb_ad_82), ($ff_fd_c6_8c), ($ff_ff_f7_99),
+      ($ff_c6_df_9c), ($ff_a4_d4_9d), ($ff_81_ca_9d), ($ff_7b_cd_c9),
+      ($ff_6c_cf_f7), ($ff_7c_a6_d8), ($ff_82_93_ca), ($ff_88_81_be),
+      ($ff_a2_86_bd), ($ff_bc_8c_bf), ($ff_f4_9b_c1), ($ff_f5_99_9d)
+    ),
+
+    ( // 3
+      ($ff_f1_6c_4d), ($ff_f6_8e_54), ($ff_fb_af_5a), ($ff_ff_f4_67),
+      ($ff_ac_d3_72), ($ff_7d_c4_73), ($ff_39_b7_78), ($ff_17_bc_b4),
+      ($ff_00_bf_f3), ($ff_43_8c_cb), ($ff_55_73_b7), ($ff_5e_5c_a7),
+      ($ff_85_5f_a8), ($ff_a7_63_a9), ($ff_ef_6e_a8), ($ff_f1_6d_7e)
+    ),
+
+    ( // 4
+      ($ff_ee_1d_24), ($ff_f1_65_22), ($ff_f7_94_1d), ($ff_ff_f1_00),
+      ($ff_8f_c6_3d), ($ff_37_b4_4a), ($ff_00_a6_50), ($ff_00_a9_9e),
+      ($ff_00_ae_ef), ($ff_00_72_bc), ($ff_00_54_a5), ($ff_2f_31_92),
+      ($ff_65_2c_91), ($ff_91_27_8f), ($ff_ed_00_8c), ($ff_ee_10_5a)
+    ),
+
+    ( // 5
+      ($ff_9d_0a_0f), ($ff_a1_41_0d), ($ff_a3_62_09), ($ff_ab_a0_00),
+      ($ff_58_85_28), ($ff_19_7b_30), ($ff_00_72_36), ($ff_00_73_6a),
+      ($ff_00_76_a4), ($ff_00_4a_80), ($ff_00_33_70), ($ff_1e_14_64),
+      ($ff_45_0e_61), ($ff_62_05_5f), ($ff_9d_00_5c), ($ff_9d_00_39)
+    ),
+
+    ( // 6
+      ($ff_79_00_00), ($ff_7b_30_00), ($ff_7c_49_00), ($ff_82_7a_00),
+      ($ff_3e_66_17), ($ff_04_5f_20), ($ff_00_58_24), ($ff_00_59_51),
+      ($ff_00_5b_7e), ($ff_00_35_62), ($ff_00_20_56), ($ff_0c_00_4b),
+      ($ff_31_00_4a), ($ff_4b_00_48), ($ff_7a_00_45), ($ff_7a_00_26)
+    ),
+
+    ( // 7
+      ($ff_c7_b1_98), ($ff_9a_85_75), ($ff_72_63_57), ($ff_52_48_42),
+      ($ff_37_30_2d), ($ff_c6_9c_6d), ($ff_a7_7c_50), ($ff_8c_62_3a),
+      ($ff_74_4b_24), ($ff_61_38_13), ($ff_00_07_43), ($ff_00_00_38),
+      ($ff_27_00_37), ($ff_38_00_35), ($ff_67_00_32), ($ff_67_00_13)
+    )
+  );
+begin
+  Result := COLORS[ARow, ACol];
+end;
+
+// カーソル
 
 { TSelectorCursor }
 
@@ -846,7 +1236,8 @@ end;
 
 procedure TSelectorCursor.Update(const ASize: Single);
 begin
-  Stroke.Thickness := Width * CURSOR_THICKNESS_RATIO;
+  Stroke.Thickness := 
+    Max(CUSOR_THICKNESS_MINIMUM_SIZE, Width * CURSOR_THICKNESS_RATIO);
 
   Width := ASize;
   Height := ASize;
@@ -953,8 +1344,8 @@ begin
     // 有効範囲（グラデーション矩形）にクランプ
     var R := LocalRect.Round;
 
-    X := AX;//EnsureRange(AX, R.Left, R.Right);
-    Y := AY;//EnsureRange(AY, R.Top, R.Bottom);
+    X := EnsureRange(AX, R.Left, R.Right);
+    Y := EnsureRange(AY, R.Top, R.Bottom);
 
     var W := Max(1.0, R.Width  - 1);
     var H := Max(1.0, R.Height - 1);
@@ -971,16 +1362,14 @@ begin
     begin
       // 上半分 : V=1, S=0→1
       FVal := 1;
-      FSat := EnsureRange(NY / 0.5, 0, 1);
+      FSat := EnsureRange(NY * 2, 0, 1);
     end
     else
     begin
       // 下半分 : S=1, V=1→0
       FSat := 1;
-      FVal := EnsureRange(1 - (NY - 0.5) / 0.5, 0, 1);
+      FVal := EnsureRange(1 - (NY - 0.5) * 2, 0, 1);
     end;
-
-    Log.d([FHue]);
 
     CalcColor;
   end;
@@ -990,6 +1379,20 @@ begin
     Y - Height / 2,
     Width,
     Height);
+end;
+
+{ TCellCursor }
+
+procedure TCellCursor.MoveTo(const AX, AY: Single);
+begin
+  with TCustomCellSelector(FSelector) do
+  begin
+    var Col, Row: Integer;
+    CellFromPoint(AX, AY, Col, Row);
+    SetSelectedCell(Col, Row);
+  end;
+
+  Visible := True;
 end;
 
 end.
--- uMain.pas
+++ uMain.pas
@@ -17,6 +17,8 @@ type
   private
     FCircleSelector: TCircleSelector;
     FRectSelector: TRectSelector;
+    F16Selector: T16CellSelector;
+    F128Selector: T128CellSelector;
   public
     procedure SelectorChange(Sender: TObject; const AColor: TAlphaColor);
   end;
@@ -42,16 +44,26 @@ end;
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   FCircleSelector := TCircleSelector.Create(Self);
-  FCircleSelector.Parent := Self;
   FCircleSelector.OnChange := SelectorChange;
+  FCircleSelector.Parent := Self;
 
   Image1.Bitmap.SetSize(Trunc(Image1.Width), Trunc(Image1.Height));
 
   FRectSelector := TRectSelector.Create(Self);
   FRectSelector.Position.X := 500;
   FRectSelector.OnChange := SelectorChange;
-
   FRectSelector.Parent := Self;
+
+  F16Selector := T16CellSelector.Create(Self);
+  F16Selector.Position.Y := 330;
+  F16Selector.OnChange := SelectorChange;
+  F16Selector.Parent := Self;
+
+  F128Selector := T128CellSelector.Create(Self);
+  F128Selector.Position.X := 500;
+  F128Selector.Position.Y := 330;
+  F128Selector.OnChange := SelectorChange;
+  F128Selector.Parent := Self;
 
   //Log.d('');
 end;
@@ -65,6 +77,13 @@ procedure TForm1.SelectorChange(Sender: TObject; const AColor: TAlphaColor);
 begin
   //Log.d(IntToHex(AColor, 8));
   Image1.Bitmap.Clear(AColor);
+
+  for
+    var S: TCustomSelector in
+    [FCircleSelector, FRectSelector, F16Selector, F128Selector]
+  do
+    if S <> Sender then
+      S.SetColorWihtoutEvent(AColor);
 end;
 
 end.
